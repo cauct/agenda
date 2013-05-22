8f24b27839baf7f20f32a9e1657a072637ca29d7
Claudia Tedesco <tedesco.claudia@gmail.com>
2013-05-22 16:26:14 -0300

    teste do insert

--- src/test/java/br/gov/serpro/agenda/business/CompromissoBCTest.java
+++ src/test/java/br/gov/serpro/agenda/business/CompromissoBCTest.java
@@ -13,31 +13,17 @@ import org.junit.runner.RunWith;
 import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
 import br.gov.serpro.agenda.domain.Compromisso;
 
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//
-//import java.util.List;
-//
-//import javax.inject.Inject;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
-//import br.gov.serpro.compromisso.domain.Compromisso;
-//
 @RunWith(DemoiselleRunner.class)
 public class CompromissoBCTest {
 
 	@Inject
 	private CompromissoBC compromissoBC;
 	
-	@Before
+/*	@Before
 	public void before() {
 		for (Compromisso compromisso : compromissoBC.findAll()) {
 			compromissoBC.delete(compromisso.getId());
+			
 		}
 	}
 
@@ -47,45 +33,47 @@ public class CompromissoBCTest {
 		List<Compromisso> listaCompromisso = compromissoBC.findAll();
 		assertNotNull(listaCompromisso);
 		assertEquals(10, listaCompromisso.size());
+	}*/
+	
+	
+	@Test
+	public void testInsert() {
+		Compromisso compromisso = new Compromisso();
+		compromissoBC.insert(compromisso);
+		List<Compromisso> listacompromissos = compromissoBC.findAll();
+		assertNotNull(listacompromissos);
+		assertEquals(1, listacompromissos.size());
 	}
-//	
-//	@Test
-//	public void testInsert() {
-//		Compromisso compromisso = new Compromisso();
-//		compromissoBC.insert(compromisso);
-//		List<compromisso> listacompromissos = compromissoBC.findAll();
-//		assertNotNull(listacompromissos);
-//		assertEquals(1, listacompromissos.size());
-//	}
-//	
-//	@Test
-//	public void testDelete() {
-//		compromisso compromisso = new compromisso("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
-//		compromissoBC.insert(compromisso);
-//		
-//		List<compromisso> listacompromissos = compromissoBC.findAll();
-//		assertNotNull(listacompromissos);
-//		assertEquals(1, listacompromissos.size());
-//		
-//		compromissoBC.delete(compromisso.getId());
-//		listacompromissos = compromissoBC.findAll();
-//		assertEquals(0, listacompromissos.size());
-//	}
-//	@Test
-//	public void testUpdate() {
-//		compromisso compromisso = new compromisso("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
-//		compromissoBC.insert(compromisso);
-//		
-//		List<compromisso> listacompromissos = compromissoBC.findAll();
-//		compromisso compromisso2 = (compromisso)listacompromissos.get(0);
-//		assertNotNull(listacompromissos);
-//		assertEquals("Demoiselle Portal", compromisso2.getDescription());
-//		
-//		compromisso2.setDescription("Demoiselle Portal alterado");
-//		compromissoBC.update(compromisso2);
-//		
-//		listacompromissos = compromissoBC.findAll();
-//		compromisso compromisso3 = (compromisso)listacompromissos.get(0);
-//		assertEquals("Demoiselle Portal alterado", compromisso3.getDescription());
-//	}
+	/*	
+	@Test
+	public void testDelete() {
+		compromisso compromisso = new compromisso("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
+		compromissoBC.insert(compromisso);
+		
+		List<compromisso> listacompromissos = compromissoBC.findAll();
+		assertNotNull(listacompromissos);
+		assertEquals(1, listacompromissos.size());
+		
+		compromissoBC.delete(compromisso.getId());
+		listacompromissos = compromissoBC.findAll();
+		assertEquals(0, listacompromissos.size());
+	}
+	@Test
+	public void testUpdate() {
+		compromisso compromisso = new compromisso("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br");
+		compromissoBC.insert(compromisso);
+		
+		List<compromisso> listacompromissos = compromissoBC.findAll();
+		compromisso compromisso2 = (compromisso)listacompromissos.get(0);
+		assertNotNull(listacompromissos);
+		assertEquals("Demoiselle Portal", compromisso2.getDescription());
+		
+		compromisso2.setDescription("Demoiselle Portal alterado");
+		compromissoBC.update(compromisso2);
+		
+		listacompromissos = compromissoBC.findAll();
+		compromisso compromisso3 = (compromisso)listacompromissos.get(0);
+		assertEquals("Demoiselle Portal alterado", compromisso3.getDescription());
+	}
+*/
 }
